20ba69eb3bb24810a863fd67ee83aac1fc9b6219
Brandon Calimares <brandoncalimares@gmail.com>
2023-07-07 20:30:54 -0300

    Cambios esteticos y agregando seccion de preguntas frecuentes

--- js/comments.js
+++ js/comments.js
@@ -1,4 +1,3 @@
-
 arrowLeft = document.querySelector(".comments__arrow__container-left");
 arrowRight = document.querySelector(".comments__arrow__container-right");
 sliderContainer = document.querySelector(".comments__slider");
@@ -7,27 +6,25 @@ sliderElements = document.querySelectorAll(".comment__element");
 let position = 0;
 
 arrowLeft.addEventListener("click", () => {
-
-    sliderElements[position].classList.remove("comment__element-active")
-    position = correctPosition(position - 1)
-    sliderElements[position].classList.add("comment__element-active")
-    sliderContainer.scrollLeft -=800;
-})
+  sliderElements[position].classList.remove("comment__element-active");
+  position = correctPosition(position - 1);
+  sliderElements[position].classList.add("comment__element-active");
+  sliderContainer.scrollLeft -= 800;
+});
 
 arrowRight.addEventListener("click", () => {
-
-    sliderElements[position].classList.remove("comment__element-active")
-    position = correctPosition(position + 1)
-    sliderElements[position].classList.add("comment__element-active")
-    sliderContainer.scrollLeft +=800;
-})
+  sliderElements[position].classList.remove("comment__element-active");
+  position = correctPosition(position + 1);
+  sliderElements[position].classList.add("comment__element-active");
+  sliderContainer.scrollLeft += 800;
+});
 
 function correctPosition(position) {
-    console.log(position)
-    if (position < 0) {
-        position = 0;
-    } else if (position > sliderElements.length - 1) {
-        position = sliderElements.length - 1;
-    }
-    return position
+  console.log(position);
+  if (position < 0) {
+    position = 0;
+  } else if (position > sliderElements.length - 1) {
+    position = sliderElements.length - 1;
+  }
+  return position;
 }
