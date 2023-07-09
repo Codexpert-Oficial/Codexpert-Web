c9b952f3c766dd67b4b536022f9e2b43f0d50a38
Brandon Calimares <brandoncalimares@gmail.com>
2023-07-09 20:52:49 -0300

    Añadiendo funcionalidad de flechas desabilitadas en la seccion de comentarios

--- js/comments.js
+++ js/comments.js
@@ -10,6 +10,12 @@ arrowLeft.addEventListener("click", () => {
   position = correctPosition(position - 1);
   sliderElements[position].classList.add("comment__element-active");
   sliderContainer.scrollLeft -= 800;
+  arrowRight.classList.remove("comments__arrow__container-disabled");
+  if (position == 0) {
+    arrowLeft.classList.add("comments__arrow__container-disabled");
+  } else {
+    arrowLeft.classList.remove("comments__arrow__container-disabled");
+  }
 });
 
 arrowRight.addEventListener("click", () => {
@@ -17,6 +23,12 @@ arrowRight.addEventListener("click", () => {
   position = correctPosition(position + 1);
   sliderElements[position].classList.add("comment__element-active");
   sliderContainer.scrollLeft += 800;
+  arrowLeft.classList.remove("comments__arrow__container-disabled");
+  if (position == sliderElements.length - 1) {
+    arrowRight.classList.add("comments__arrow__container-disabled");
+  } else {
+    arrowRight.classList.remove("comments__arrow__container-disabled");
+  }
 });
 
 function correctPosition(position) {
