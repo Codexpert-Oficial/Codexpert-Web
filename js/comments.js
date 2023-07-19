445a5626a050277372038194dc8900cd3dedea3f
mathias <mathiastortomano.dev@gmail.com>
2023-07-19 18:09:21 -0300

    adaptando todas las secciones de la pagina a mobile/respoinsive

--- js/comments.js
+++ js/comments.js
@@ -1,34 +1,52 @@
-arrowLeft = document.querySelector(".comments__arrow__container-left");
-arrowRight = document.querySelector(".comments__arrow__container-right");
+
+arrowsLeft = document.querySelectorAll(".comments__arrow__container-left");
+arrowsRight = document.querySelectorAll(".comments__arrow__container-right");
 sliderContainer = document.querySelector(".comments__slider");
 sliderElements = document.querySelectorAll(".comment__element");
 
 let position = 0;
 
-arrowLeft.addEventListener("click", () => {
-  sliderElements[position].classList.remove("comment__element-active");
-  position = correctPosition(position - 1);
-  sliderElements[position].classList.add("comment__element-active");
-  sliderContainer.scrollLeft -= 800;
-  arrowRight.classList.remove("comments__arrow__container-disabled");
-  if (position == 0) {
-    arrowLeft.classList.add("comments__arrow__container-disabled");
-  } else {
-    arrowLeft.classList.remove("comments__arrow__container-disabled");
-  }
+
+arrowsLeft.forEach(arrowLeft => {
+  arrowLeft.addEventListener("click", () => {
+    sliderElements[position].classList.remove("comment__element-active");
+    position = correctPosition(position - 1);
+    sliderElements[position].classList.add("comment__element-active");
+
+    let getWidthELement = window.getComputedStyle(sliderElements[position]);
+    let WidthELement = getWidthELement.width.slice(0, -2);
+    sliderContainer.scrollLeft -= Number(WidthELement);
+
+    arrowRight = arrowLeft.parentElement.querySelector(".comments__arrow__container-right");
+    arrowRight.classList.remove("comments__arrow__container-disabled");
+
+    if (position == 0) {
+      arrowLeft.classList.add("comments__arrow__container-disabled");
+    } else {
+      arrowLeft.classList.remove("comments__arrow__container-disabled");
+    }
+  });
 });
 
-arrowRight.addEventListener("click", () => {
-  sliderElements[position].classList.remove("comment__element-active");
-  position = correctPosition(position + 1);
-  sliderElements[position].classList.add("comment__element-active");
-  sliderContainer.scrollLeft += 800;
-  arrowLeft.classList.remove("comments__arrow__container-disabled");
-  if (position == sliderElements.length - 1) {
-    arrowRight.classList.add("comments__arrow__container-disabled");
-  } else {
-    arrowRight.classList.remove("comments__arrow__container-disabled");
-  }
+arrowsRight.forEach(arrowRight => {
+  arrowRight.addEventListener("click", () => {
+    sliderElements[position].classList.remove("comment__element-active");
+    position = correctPosition(position + 1);
+    sliderElements[position].classList.add("comment__element-active");
+
+    let getWidthELement = window.getComputedStyle(sliderElements[position]);
+    let WidthELement = getWidthELement.width.slice(0, -2);
+    sliderContainer.scrollLeft += Number(WidthELement);
+
+    arrowLeft = arrowRight.parentElement.querySelector(".comments__arrow__container-left");
+    arrowLeft.classList.remove("comments__arrow__container-disabled");
+
+    if (position == sliderElements.length - 1) {
+      arrowRight.classList.add("comments__arrow__container-disabled");
+    } else {
+      arrowRight.classList.remove("comments__arrow__container-disabled");
+    }
+  });
 });
 
 function correctPosition(position) {
@@ -40,3 +58,6 @@ function correctPosition(position) {
   }
   return position;
 }
+
+
+
--- js/nav.js
+++ js/nav.js
@@ -7,3 +7,38 @@ swtichButton.addEventListener("click", (e) => {
     e.preventDefault();
     switchCircle.classList.toggle("switch__circle-active")
 })
+
+
+
+
+// nav
+
+
+let barsMenuOpen = document.querySelector(".nav__menu__bars-open");
+let barsMenuClose = document.querySelector(".nav__menu__bars-close");
+let navContainerMobile = document.querySelector(".nav__container-mobile");
+let allNavLinks = document.querySelectorAll(".nav__element__link");
+
+function closeMenu() {
+    navContainerMobile.style.left = "1000px";
+    barsMenuClose.style.display = "none";
+    barsMenuOpen.style.display = "flex";
+}
+
+barsMenuOpen.addEventListener("click", (e) => {
+    e.preventDefault();
+    navContainerMobile.style.left = "0px";
+    barsMenuOpen.style.display = "none";
+    barsMenuClose.style.display = "flex";
+})
+
+barsMenuClose.addEventListener("click", (e) => {
+    e.preventDefault();
+    closeMenu();
+})
+
+allNavLinks.forEach((linkNav) => {
+    linkNav.addEventListener("click", () => {
+        closeMenu();
+    })
+});
